7a3ce2809a2432b2f2b253e691b9e3830cf1e0c7
Canter <canterpurity@proton.me>
2025-09-05 04:49:22 -0400

    added concat and print_rope

--- rope.c
+++ rope.c
@@ -12,15 +12,60 @@ typedef struct rope_node{
 rope_node *make_leaf(char *str){
     rope_node *leaf = malloc(sizeof(rope_node));
     leaf->str = malloc(strlen(str)+1);
-    strcpy(leaf->str,str);
+    leaf->str = strdup(str);
     leaf->weight = strlen(str);
     leaf->left = leaf->right = NULL;
     return leaf;
 }
+
+int length(rope_node *node){
+    if (node->str != NULL){
+        return node->weight;
+    }else{
+        return node->weight + length(node->right);
+    }
+}
+
+rope_node *concat(rope_node *left,rope_node *right){
+    rope_node *node = malloc(sizeof(rope_node));
+    node->left = left;
+    node->right = right;
+    node->str = NULL;
+    node->weight = length(left);
+    return node;
+}
+
+void print_rope(rope_node *node) {
+    if (node == NULL)return;
+    if (node->str != NULL){
+        printf("%s",node->str);
+    }
+    print_rope(node->left);
+    print_rope(node->right);
+}
+
 int main (){
-    char *str = "hello\0";
-    rope_node *node = make_leaf(str);
-    printf("%s world\n",node->str);
-    free(node->str);free(node);
+    FILE *f = fopen("test2.txt", "r");
+    if(!f){
+        perror("failed to open file!");
+        return 1;
+    }
+    char buffer[CHUNK_SIZE + 1];
+    size_t bytes_read;
+
+    rope_node *root = NULL;
+    while ((bytes_read = fread(buffer, 1, CHUNK_SIZE, f)) > 0) {
+        buffer[bytes_read] = '\0';
+        rope_node *leaf = make_leaf(buffer);
+
+        if(root == NULL){
+            root = leaf;
+        }else {
+            root = concat(root,leaf);
+        }
+    }
+
+    print_rope(root);
+    printf("%d",root->weight);
     return 0;
 }
